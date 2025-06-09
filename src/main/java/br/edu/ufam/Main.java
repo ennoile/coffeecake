51b457e92f69250ffff512282b1a7cac2d270637
Waleson Melo <walesonmelo23@gmail.com>
2025-06-09 08:22:41 -0400

    adiciona tela de login e controlador correspondente

--- src/main/java/br/edu/ufam/Main.java
+++ src/main/java/br/edu/ufam/Main.java
@@ -1,50 +1,32 @@
 package br.edu.ufam;
 
-import java.util.Scanner;
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import br.edu.ufam.service.AutenticacaoService;
-import br.edu.ufam.view.IngredienteView;
-import br.edu.ufam.view.ProdutoView;
-import br.edu.ufam.view.UsuarioView;
+public class Main extends Application {
+    private static Scene scene;
 
-public class Main {
+    @Override
+    public void start(Stage stage) throws IOException {
+        scene = new Scene(loadFXML("login"), 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("controller/" + fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
     public static void main(String[] args) {
-        AutenticacaoService autenticacaoService = new AutenticacaoService();
-
-        while (!autenticacaoService.isAutenticado()) {
-            autenticacaoService.login();
-        }
-
-        Scanner scanner = new Scanner(System.in);
-        int opcao = 0;
-
-        while (true) {
-            System.out.println("-------COFFECAKE-------");
-            System.out.println("| 1 - Usuario         |");
-            System.out.println("| 2 - Ingrediente     |");
-            System.out.println("| 3 - Produto |");
-            System.out.println("| 0 - Sair            |");
-            System.out.print("Opção: ");
-            opcao = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (opcao) {
-                case 1:
-                    UsuarioView.executar();
-                    break;
-                case 2:
-                    IngredienteView.executar();
-                    break;
-                case 3:
-                    ProdutoView.executar();
-                    break;
-                case 0:
-                    scanner.close();
-                    autenticacaoService.logout();
-                    System.exit(0);
-                default:
-                    break;
-            }
-        }
+        launch();
     }
 }
---
+++ src/main/java/br/edu/ufam/controller/LoginController.java
@@ -0,0 +1,12 @@
+package br.edu.ufam.controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class LoginController {
+    @FXML
+    private void testeClick2() throws IOException {
+        System.out.println("aaa");
+    }
+}
